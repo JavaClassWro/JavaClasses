886a646d2509d300f7acdde795fbb681baaa369d
JavaClassWro <mer__lin@rambler.ru>
2020-11-03 20:06:14 +0100

    added Car attributes and usage

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Car bmw = new Car("BMW", "Blue");
+        System.out.println(bmw.manufacturer+" "+bmw.colour);
     }
 }
