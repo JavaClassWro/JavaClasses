040bcfeca2db2635c10d36592df0622e8a72a7dd
JavaClassWro <mer__lin@rambler.ru>
2020-11-05 19:45:36 +0100

    static, final, args examples

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,11 @@
 public class Car {
+
+    static {
+        System.out.println("Hey I'm static block in Car!");
+    }
+
+    static String static_member = "Hey, I'm static member!";
+
     public String manufacturer;
     public String colour;
     private String vin;
